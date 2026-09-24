c6481556172f7d61d2eabe51e4b4ffdec25a6cc4
awesomeness8765 <74803688+awesomeness8765@users.noreply.github.com>
2026-09-24 14:22:51 -0500

    Update CDN links for jQuery and Lodash

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 require.config({
   paths: {
-    jquery: 'http://cdn.jsdelivr.net/jquery/2.2.0/jquery.min',
-    lodash: 'http://cdn.jsdelivr.net/lodash/4.5.0/lodash.min',
+    jquery: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min',
+    lodash: 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.5.0/lodash.min',
     materialize: 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min'
   },
   shim: {
