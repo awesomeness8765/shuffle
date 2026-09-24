3be6888579e5efdf8ad4c571b84812bdbff19562
awesomeness8765 <74803688+awesomeness8765@users.noreply.github.com>
2026-09-24 14:29:26 -0500

    Add hammer.js dependency to require config

--- js/main.js
+++ js/main.js
@@ -2,11 +2,12 @@ require.config({
   paths: {
     jquery: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min',
     lodash: 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.5.0/lodash.min',
-    materialize: 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min'
+    materialize: 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min',
+    hammerjs: 'https://cdnjs.cloudflare.com/ajax/libs/hammer.js/2.0.8/hammer.min'
   },
   shim: {
     materialize: {
-      deps: ['jquery']
+      deps: ['jquery', 'hammerjs']
     }
   }
 });
